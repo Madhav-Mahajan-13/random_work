408dbbf20de95a215eacd09961519c9cb8b92fc0
MADHAV <madhavmahajan152@gmail.com>
2025-03-20 10:27:04 +0530

    basic nav

--- src/pages/messagePage.jsx
+++ src/pages/messagePage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import BasicButtonGroup from "../components/button";
 import BasicSelect from "../components/select";
+import NavTabs from "../components/navTab";
 
 const MessagePage=()=>{
     const buttonlabels=["one" ,"two"];
@@ -16,6 +17,10 @@ const MessagePage=()=>{
             <div> <BasicButtonGroup buttons={buttonlabels} /> </div>
             <div>
                 <BasicSelect options={selectlabels}/>
+            </div>
+            <div>
+                <NavTabs/>
+                
             </div>
         </div>
     )
